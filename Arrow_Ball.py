d2ec8d048123feabd4a52e2185e7aa3c392bd344
RBigley <wx8ball@gmail.com>
2018-07-24 08:43:02 -0600

    Catching up on commit

---
+++ Arrow_Ball.py
@@ -0,0 +1,5 @@
+import pygame, sys
+pygame.init()
+screen = pygame.display.set_mode([640,480])
+background = pygame.Surface(screen.get_size())
+background.fill
---
+++ soccer.py
@@ -0,0 +1,5 @@
+pl = input('Are you male -"m" or female -"f": ')
+age = float(input('How old are you?: '))
+
+if age >9 and age < 13 and pl == 'f':
+	print("You can play soccer on our team!")
